d1475ef3df18f8c1db318fabe9058918845592cb
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-08-07 16:32:28 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const allIngredients = document.querySelector('#ingredients');
+const list = document.createElement("li");
+console.log(list.textContent = `${ingredients}`);
+list.classList.add('item');
+console.log(list.classList);
+allIngredients.append(list);
+
+
+
+
+
+
+
+
