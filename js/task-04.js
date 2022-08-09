642e027ae064fc338812317a8a57166070d1bca7
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-08-09 20:08:24 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+const button = document.querySelector('.button');
+
+  const buttonDecrement = document.querySelector('[data-action="decrement"]');
+  const buttonIncrement = document.querySelector('[data-action="increment"]');
+
+  buttonDecrement.addEventListener("click",  ()=> {
+      counterValue -= 1;
+      document.getElementById('value').innerHTML = counterValue;
+  });
+  
+  buttonIncrement.addEventListener("click",  ()=> {
+      counterValue += 1;
+      document.getElementById('value').innerHTML = counterValue;
+  })
+
+
+
