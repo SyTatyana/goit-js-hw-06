87acd5ed03f263ad7c32d3eb60862847b2266617
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-08-10 13:18:02 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+function changingColor(){
+  const colorOfBackground = document.querySelector(".color");
+  colorOfBackground.textContent = (getRandomHexColor(''));
+  document.body.style.backgroundColor=(getRandomHexColor(""));
 }
+const button = document.querySelector(".change-color").addEventListener('click', changingColor );
+  
+  
