a8bb8933d4644f52456bfce8ff41e3685117bbe2
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-08-14 17:11:20 +0300

    task-10

--- js/task-05.js
+++ js/task-05.js
@@ -1,21 +1,10 @@
-/*const textInput = document.querySelector("#name-input");
+const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-function fillIn(event) {
-    if(textInput !== ''){
-        output.textContent = event.currentTarget.value;
-    }
-    else { output.textContent = 'Anonymous';}
-}
+input.addEventListener("input", (newInput) => {
+  output.textContent = newInput.currentTarget.value;
 
-textInput.addEventListener("input", (fillIn));*/
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-input.addEventListener('input', (newInput) => {
-    output.textContent = newInput.currentTarget.value;
-
-    if (newInput.currentTarget.value === '') {
-        output.textContent = 'Anonymus';
-    };
+  if (newInput.currentTarget.value === "") {
+    output.textContent = "Anonymus";
+  }
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector("input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document
+  .querySelector("[data-destroy]")
+  .addEventListener(`click`, destroyBoxes);
+const boxes = document.querySelector("#boxes");
+
+createButton.addEventListener("click", (createButtonClick) => {
+  createButtonClick.preventDefault();
+
+  for (let i = 0; i < input.value; i += 1) {
+    let size = 30;
+    const box = document.createElement("div");
+    box.style.width = `${(size += 10)}px`;
+    box.style.height = `${(size += 10)}px`;
+    box.style.background = `${getRandomHexColor()}`;
+    boxes.append(box);
+  }
+  input.value = "";
+});
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
